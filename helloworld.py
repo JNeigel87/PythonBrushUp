1b9c6d8f23f52976f591eeefbf0292e6b48316a4
JNeigel87 <69132959+JNeigel87@users.noreply.github.com>
2021-10-13 00:09:33 -0500

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+# 1. Print "Hello World".
+print("Hello Coding World!")
+
+# 2. print "Hello Noelle!" with the name in a variable.
+name = "JR"
+print("Hello", name)
+print("Hello " + name)
+
+# 3.print "Hello 42!" with the number in a variable
+x = "17"
+print("Hello", x)
+print("Hello number " + x)
+print("****commented error out to reveal the rest of the code****")
+
+# 4. print "I love to eat sushi and pizza." with the foods in variables.
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("My favorite foods are {} and {}.".format(fave_food1, fave_food2))
+print(f"My favorite foods are {fave_food2} and {fave_food1}!")
